cfc297a6efed3d5b57c17934db8bf2ba2c07823a
Ana Nascimento <anacdon@yahoo.com.br>
2021-10-21 23:07:05 -0300

    ajeitar pag create

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,15 +1,18 @@
 class RestaurantsController < ApplicationController
-  before_action :set_restaurant, except: :index
+  before_action :set_restaurant, only: :show
 
   def index
     @restaurants = Restaurant.all
   end
 
   def new
-    @restaurant = Restaurant.new()
+    @restaurant = Restaurant.new
   end
 
   def create
+    @restaurant = Restaurant.new(restaurant_params)
+    @restaurant.save
+    redirect_to restaurant_path(@restaurant)
   end
 
   def show
@@ -22,6 +25,6 @@ class RestaurantsController < ApplicationController
   end
 
   def set_restaurant
-    @restaurant = Restaurant.find(restaurant)
+    @restaurant = Restaurant.find(params[:id])
   end
 end
